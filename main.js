292a3e49abb497fe4240f090544920d75e9e4896
Middleway13 <middlewayonlinemedia@gmail.com>
2021-05-20 08:41:06 -0400

    new files updated: index, css and js

---
+++ main.js
@@ -0,0 +1,124 @@
+// let output = document.querySelector('.output')
+
+
+document.querySelector('.button').addEventListener('click', function (event) {
+    console.log(event.target)
+})
+
+// document.querySelector('#equal').addEventListener('click', function (event) {
+//     console.log(eval(output.innerText)
+// })
+
+// document.querySelector('#equal').addEventListener('click', function (event) {
+//     output = ""
+//     let equals = document.createTextNode(eval(output.innerText))
+//     document.getElementById("output").innerHTML = ""
+//     newElement.appendChild(equals)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#one').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#two').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#three').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#four').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#five').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#six').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#seven').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#eight').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#nine').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#point').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#add').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#divide').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#multi').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#zero').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
+
+// document.querySelector('#sub').addEventListener('click', function (event){
+//     let newEntry = document.createTextNode(event.target.innerHTML)
+//     newElement.appendChild(newEntry)
+//     output.appendChild(newElement)
+//     console.log(event.target.innerHTML)
+// })
