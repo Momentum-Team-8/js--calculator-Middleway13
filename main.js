4f4d24bfd78e8a9cc8e8de5cbdb193a9aa07a5dd
Middleway13 <middlewayonlinemedia@gmail.com>
2021-07-14 13:22:16 -0400

    new files

--- main.js
+++ main.js
@@ -1,127 +1,25 @@
-let output = document.querySelector(".output");
+// Varible that grabs the <div> class id 
+let output = document.querySelector("#output");
+// Varible that will be called on to help create a new element 
 let newElement = document.createElement("h1");
 
+// Document method that tracks any clicks made on the DOM
 document.addEventListener("click", function (event) {
-  console.log(event.target.innerHTML)
-  let newEntry = document.createTextNode(event.target.innerHTML)
-  newElement.appendChild(newEntry)
-  output.appendChild(newElement)
-})
+// When a click is made, the following code will run.
 
-// document.querySelector('#equal').addEventListener('click', function (event) {
-//     console.log(eval(output.innerText)
-// })
+// Varible that is called to create node within exsiting DOM element
+  let newEntry = document.createTextNode(event.target.innerHTML);
 
-// document.querySelector('#equal').addEventListener('click', function (event) {
-//     output = ""
-//     let equals = document.createTextNode(eval(output.innerText))
-//     document.getElementById("output").innerHTML = ""
-//     newElement.appendChild(equals)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
+  // Method that appends a child element to the "newEntry" varible
+  newElement.appendChild(newEntry);
+  output.appendChild(newElement);
+});
 
-// document.querySelector('#one').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#two').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#three').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#four').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#five').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#six').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#seven').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#eight').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#nine').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#point').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#add').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#divide').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#multi').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#zero').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
-
-// document.querySelector('#sub').addEventListener('click', function (event){
-//     let newEntry = document.createTextNode(event.target.innerHTML)
-//     newElement.appendChild(newEntry)
-//     output.appendChild(newElement)
-//     console.log(event.target.innerHTML)
-// })
+//Method that will output "COOL" when element with "#equal" is clicked
+document.querySelector("#equal").addEventListener("click", function (event) {
+  output = "cool";
+  let equals = document.createTextNode(eval(output.innerText));
+  document.getElementById("output").innerHTML = "COOL";
+  newElement.appendChild(equals);
+  output.appendChild(newElement);
+});
